99f18a8bfe2c4ba026380cb7e09353a9be347974
cynthia <cxnthiazen12@gmail.com>
2026-03-27 16:44:32 +0700

    Add player power-up state and enemy knockback
    Introduce a hasPowerUp boolean to PlayerController and add OnCollisionEnter logic to apply an impulse to Enemy rigidbodies when the player has the power-up. Update the FloatingIsland scene to serialize the new hasPowerUp field and to set a focalPoint reference (plus minor prefab/transform references). Enables a power-up mechanic that knocks enemies away on collision.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,6 +13,8 @@ public class PlayerController : MonoBehaviour
     private InputAction smashAction;
     private InputAction breakAction;
 
+    public bool hasPowerUp = false;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
@@ -32,5 +34,32 @@ public class PlayerController : MonoBehaviour
         {
             rb.linearVelocity = Vector3.zero;
         }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            if (hasPowerUp == true)
+            {
+                var rb = collision.gameObject.GetComponent<Rigidbody>();
+                var direction = collision.transform.position - transform.position;
+                rb.AddForce(100 * direction.normalized , ForceMode.Impulse);
+            }
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("PowerUp"))
+        {
+            hasPowerUp = true;
+            Destroy(other.gameObject);
+
+            StartCoroutine(PowerUpCountdown());
+        }
+    }
+    IEnumerator PowerUpCountdown()
+    {
+        yield return new WaitForSeconds(10);
+        hasPowerUp = false;
     }
 }
